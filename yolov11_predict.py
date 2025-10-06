cc5f7189d645d432af367a7b8c227933ebd7705d
UltralyticsAssistant <web@ultralytics.com>
2025-10-06 04:26:46 +0000

    Auto-format by https://ultralytics.com/actions

--- yolov11_predict.py
+++ yolov11_predict.py
@@ -1,12 +1,14 @@
-from ultralytics import YOLO
 import keyboard
+
+from ultralytics import YOLO
+
 # 加载预训练的 YOLOv11n 模型
-model = YOLO('yolo11n.pt')
-source = 'E:\\ultralytics-main\\csreen.png' #更改为自己的图片路径
-#通过print(model.names)可以查看所有的类别名
+model = YOLO("yolo11n.pt")
+source = "E:\\ultralytics-main\\csreen.png"  # 更改为自己的图片路径
+# 通过print(model.names)可以查看所有的类别名
 print(model.names)
 # 运行推理，并附加参数
-model.predict(source, save=True,show=True)
+model.predict(source, save=True, show=True)
 while True:
-    if keyboard.is_pressed('p'):
+    if keyboard.is_pressed("p"):
         break
